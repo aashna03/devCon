a211a7014582337fd8231593bb718940a430376b
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-03-25 18:24:01 +0530

    sequence of routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ const express = require("express");
 
 const app = express();
 
+// this will overwrite other /....
+// app.use("/", (req, res) => {
+//     res.send("Hello from dashboard")
+// })
+
 // request handler
 // route, (req, res), ()=>{}
+// anything "/test/dfsdfsdbsa/fgfd" matches here not "/test3456" donot matches kindof wildcard
 app.use("/test", (req, res) => {
     res.send("Hello from test endpoint!!!!")
 })
 
+// this works as sequence matters
 app.use("/", (req, res) => {
     res.send("Hello from dashboard")
 })
