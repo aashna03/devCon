586245b2250b8997ea2f5f8d1829c0d3248de15f
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-03-26 19:46:49 +0530

    added database connection and user model

--- src/app.js
+++ src/app.js
@@ -1,9 +1,51 @@
 const express = require("express");
+require('dotenv').config();
 
+const {connectDB} =  require("./config/database.js")
 const app = express();
 
+const User = require("./models/user");
+
+app.post("/signup", async(req, res) => {
+
+    // const userObj = {
+    //     firstName: "Aashna",
+    //     lastName: "Agarwal",
+    //     emailId:"aashna@agarwal.com",
+    //     password: "aashna@a6"
+    // }
+    // // creating a new instance of User model -- create new user with the above data 
+    // const user = new User(userObj);
+
+    const user = new User({
+        firstName: "Aashna",
+        lastName: "Agarwal",
+        emailId:"aashna@agarwal.com",
+        password: "aashna@a6"
+    })
+
+    try{
+        // save data in database
+        await user.save();
+        res.send("User added successfully")
+    } catch(err){
+        res.status(400).send(`Error saving the  user ${err.message}`)
+    }
+
+})
 
 
-app.listen(3000, () => {
-    console.log("server running successfully");
-});
+
+connectDB()
+    .then(() => {
+        console.log("Database connection established");
+        // we should connect to db then start listening
+        app.listen(3000, () => {
+            console.log("server running successfully");
+        });
+    })
+    .catch((err) => {
+        console.error("Database cannot be connected");
+    })
+
+
