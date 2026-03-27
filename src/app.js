fb51c1af034ea6550c986a0e1e43eea03a37fad1
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-03-27 13:38:17 +0530

    implement user management routes: signup, get user by email, feed, delete, and update

--- src/app.js
+++ src/app.js
@@ -6,8 +6,13 @@ const app = express();
 
 const User = require("./models/user");
 
+// this is a middleware which will be activated for all the routes
+// to covert json incoming coverts it into JS object and adds it into the request body again
+app.use(express.json());
+
 app.post("/signup", async(req, res) => {
 
+    // this is JS object
     // const userObj = {
     //     firstName: "Aashna",
     //     lastName: "Agarwal",
@@ -17,12 +22,14 @@ app.post("/signup", async(req, res) => {
     // // creating a new instance of User model -- create new user with the above data 
     // const user = new User(userObj);
 
-    const user = new User({
-        firstName: "Aashna",
-        lastName: "Agarwal",
-        emailId:"aashna@agarwal.com",
-        password: "aashna@a6"
-    })
+    // const user = new User({
+    //     firstName: "Aashna",
+    //     lastName: "Agarwal",
+    //     emailId:"aashna@agarwal.com",
+    //     password: "aashna@a6"
+    // })
+
+    const user = new User(req.body)
 
     try{
         // save data in database
@@ -34,6 +41,59 @@ app.post("/signup", async(req, res) => {
 
 })
 
+// get user by email
+app.get("/user", async(req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        // get user by email
+        const user = await User.find({ emailId: userEmail })
+        if(user.length === 0){
+            res.status(404).send("User Not Found")
+        }
+        res.send(user)
+
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// Feed API - GET/feed - get all users from the database
+app.get("/feed", async(req, res) => {
+    try{
+        const users = await User.find({})
+        res.send(users)
+    }
+    catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// delete user by id
+app.delete("/user", async(req, res) => {
+    const userId = req.body.userId;
+    try{
+        // const user = await User.findByIdAndDelete({ _id: userId });
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted Successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+
+// update user of the user
+app.patch("/user", async(req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try{
+        await User.findByIdAndUpdate({ _id: userId}, data);
+        res.send("User updated successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
 
 
 connectDB()
