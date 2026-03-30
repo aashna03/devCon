38d22f02bbb8dfeaa0a3444ea6a22266a9d2823a
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-03-30 13:25:35 +0530

    edit profile API

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -3,13 +3,36 @@ const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth.js");
 // userAuth attaches the user object to the request if the token is valid and user is found in the database, otherwise it sends an error response and does not call next() so the control will not come to the API handler function and it will be stopped at the middleware itself. So we can directly use req.user in our API handler function without worrying about whether it is there or not because if it is not there then the control will not come to the API handler function.
 // (req.user) to get the whole user object
+const { validateEditProfileData } = require("../utils/validation.js");
 
-// profile API
-profileRouter.get("/profile", userAuth, async(req, res) => {
+// view p+rofile API
+profileRouter.get("/profile/view", userAuth, async(req, res) => {
     try{
         const user = req.user;
         res.send(user);
     }catch(err){
+        res.status(401).send(`Error: ${err.message}`)
+    }
+})
+
+// edit profile API
+profileRouter.get("/profile/edit", userAuth, async(req, res) => {
+    try {
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid data for profile update");
+        }
+        const loggedInUser = req.user;
+        Object.keys(req.body).forEach((body_field) => (loggedInUser[body_field] = req.body[body_field]));
+        console.log(loggedInUser);
+        await loggedInUser.save();
+        // res.send("Profile updated successfully");
+        // or
+        res.json({
+            message: "Profile updated successfully",
+            data: loggedInUser
+        })
+        
+    } catch (err) {
         res.status(401).send(`Error: ${err.message}`)
     }
 })
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -15,6 +15,29 @@ const validateSignUpData = (req) => {
     }
 };
 
+const validateEditProfileData = (req) => {
+    
+    allowedEditFields = ["firstName", "lastName","photoURL", "age", "about", "gender", "skills"];
+
+    // iterates body_fields => check if it is present in allowedEditFields or not => if all body_fields are present in allowedEditFields then only it will return true otherwise false  
+    // Stops iterating immediately if the callback function returns a falsy value for any element.
+    isAllowedFields = Object.keys(req.body).every(body_field => allowedEditFields.includes(body_field));
+    return isAllowedFields;
+
+    if(!firstName){
+        console.log("firstName" +  firstName);
+        
+        throw new Error("First name is required");
+    }
+    if(!validator.isEmail(emailId)){
+        throw new Error("Invalid email address");
+    }
+    if(!validator.isStrongPassword(password)){
+        throw new Error("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol");
+    }
+};
+
 module.exports = {
     validateSignUpData,
+    validateEditProfileData
 }
