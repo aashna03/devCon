b3cf7f63abcab6ffcd8af6c8ad19d0fb8da9f186
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-04-10 21:57:14 +0530

    socekt changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -19,6 +19,7 @@ const initializeSocket = (server) =>{
                 "http://localhost:5173",
                 "http://localhost:3000",
             ],
+            methods: ["GET", "POST"],
             credentials: true,
         }
     });
